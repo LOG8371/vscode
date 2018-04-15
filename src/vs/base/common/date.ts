469ec9c24304538397c50a95c78f63bb641be3d6
Antoine Proulx <antoine@antoine-proulx.ca>
2018-04-15 14:43:41 -0400

    Add a test for toLocalISOString and extract a magic constant

--- src/vs/base/common/date.ts
+++ src/vs/base/common/date.ts
@@ -14,8 +14,12 @@ function pad(number: number): string {
 }
 
 export function toLocalISOString(date: Date): string {
+	// In JavaScript, the month of a date is between 0 and 11,
+	// so we need to add one in order to show it correctly to users
+	const MONHT_SHIFT = 1;
+
 	return date.getFullYear() +
-		'-' + pad(date.getMonth() + 1) +
+		'-' + pad(date.getMonth() + MONHT_SHIFT) +
 		'-' + pad(date.getDate()) +
 		'T' + pad(date.getHours()) +
 		':' + pad(date.getMinutes()) +
